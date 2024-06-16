83b2c4f850cba52beef8f0c49d377f6198d5983a
TheVarja <40161420+TheVarja@users.noreply.github.com>
2024-06-16 13:25:40 +0700

    Success

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project1/responsive/navbar.dart';
-
+import 'firebase_options.dart';
+/// // ...
+/// await Firebase.initializeApp(
+///   options: DefaultFirebaseOptions.currentPlatform,
+/// );
+/// ```
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
