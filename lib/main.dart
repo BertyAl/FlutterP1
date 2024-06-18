e445b585d697db9ce99168533f0b1da2a3f53a67
BertyAl <ragna989@gmail.com>
2024-06-18 21:27:17 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'WhatsApp Clone',
       theme: ThemeData(
         primaryColor: Color(0xFF075E54),// You can use your custom color
